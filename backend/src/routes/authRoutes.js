3eab2197491044ebcb2c1b72013656a9929bf08b
Sujan Parajuli <149400474+FederEx18@users.noreply.github.com>
2025-06-10 13:50:39 +0100

    Chore: Forgot password
    -created backend to help user rest password by sending email to the users

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {register, login} from "../controllers/authController.js";
+import {forgotPassword} from "../controllers/forgotPasswordController.js";
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.post("/register", register);
 
 // Rota de login
 router.post("/login", login);
+
+// Rota de recuperação de senha
+router.post("/forgot-password", forgotPassword);
 
 export default router;
